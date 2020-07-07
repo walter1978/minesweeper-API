07ec3a96af31bd03ce9137ed32b551e41150cc96
Walter <walteradrian78hotmail.com>
2020-07-07 20:26:37 -0300

    Spring Boot

---
+++ src/main/java/com/minesweeper/MinesweeperApplication.java
@@ -0,0 +1,15 @@
+package com.minesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class MinesweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperApplication.class, args);
+	}
+}
+
